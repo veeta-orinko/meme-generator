bd0b36b8c7a8aee99f08be9be389699c379bf184
Eli Bing <107372513+elizabeth-bing@users.noreply.github.com>
2022-09-02 11:48:41 +1200

    veeta additions to db

--- server/db/seeds/snacks.js
+++ server/db/seeds/snacks.js
@@ -1,5 +1,3 @@
-
-
 exports.seed = (knex) => {
   // Deletes ALL existing entries
   return knex('Snacks')
@@ -7,46 +5,52 @@ exports.seed = (knex) => {
     .then(() => {
       // Inserts seed entries
       return knex('Snacks').insert([
-          {
-            id: 201,
-            name: 'Bear-Sized Duck Pancakes',
-            image: './images/bear-sized-duck-pancakes',
-          },
-          {
-            id: 202,
-            name: 'Scrambled Moa Eggs',
-            image: './images/scrambled-moa-eggs',
-          },
-          {
-            id: 203,
-            name: 'Moa Drumsticks',
-            image: './images/moa-drumsticks',
-          },
-          {
-            id: 204,
-            name: 'Extinct Banana Cake',
-            image: './images/extinct-banana-cake',
-          },
-          {
-            id: 205,
-            name: 'Inedible Cactus',
-            image: './images/inedible-cactus',
-          },
-          {
-            id: 206,
-            name: 'Dodo Soup Surprise',
-            image: './images/dodo-soup-surprise',
-          },
-          {
-            id: 207,
-            name: 'Sheba Emu Fritters',
-            image: './images/sheba-emu-fritters',
-          },
-          {
-            id: 208,
-            name: 'Recently Extinct Platter',
-            image: './images/recently-extinct-platter',
-          },
+        {
+          id: 201,
+          name: 'Bear-Sized Duck Pancakes',
+          image: './images/bear-sized-duck-pancakes',
+        },
+        {
+          id: 202,
+          name: 'Scrambled Moa Eggs',
+          image: './images/scrambled-moa-eggs',
+        },
+        {
+          id: 203,
+          name: 'Emu & Chips',
+          image: './images/moa-and-chips',
+        },
+        {
+          id: 204,
+          name: 'Extinct Banana Cake',
+          image: './images/extinct-banana-cake',
+        },
+        {
+          id: 205,
+          name: 'Egg chowder',
+          image: './images/egg-chowder',
+        },
+        {
+          id: 206,
+          name: 'Dodo Soup Surprise',
+          image: './images/dodo-soup-surprise',
+        },
+        {
+          id: 207,
+          name: 'Shiba Pancakes',
+          image: './images/shiba-pancakes',
+        },
+        {
+          id: 208,
+          name: 'Recently Extinct Platter',
+          image: './images/recently-extinct-platter',
+        },
+
+        {
+          id: 209,
+          name: 'Cosmic Moa Cake',
+          image: './images/cosmic-moa-cake',
+        },
       ])
     })
 }
